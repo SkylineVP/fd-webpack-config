c8f3ea31064be61c35ef45d4a6a324ef86253b22
SkylineVP <vladpolizp@gmail.com>
2019-12-27 17:05:00 +0200

    build in single package

--- webpack.config/webpack.config.common.js
+++ webpack.config/webpack.config.common.js
@@ -37,7 +37,7 @@ const config = {
           {
             loader: 'file-loader',
             options: {
-              name: '[path][name].[ext]',
+              name: '[name].[ext]',
             }
           }
         ]
